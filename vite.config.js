0d2cd5420a2955c5de10a0ed7c285603d01589a3
Durga Prasanna <prasanna.guru02@gmail.com>
2025-05-09 13:53:12 +0530

    ui design update

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,10 @@
-cls
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),  tailwindcss(),],
+  base: '/aidashboard/',
+  
+})
